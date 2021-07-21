48e9742caf7ff50574b44585e08c014e4ba97d39
Santiago Pighin <pighin.santiago@gmail.com>
2021-07-21 11:12:41 +0200

    Add mlflow from le wagon lecture. Not ready to run, train implementation needed

--- taxi_rev/train.py
+++ taxi_rev/train.py
@@ -5,6 +5,64 @@ from taxi_rev.encoders import set_pipeline
 from sklearn.model_selection import train_test_split
 
 
+from memoized_property import memoized_property
+
+import mlflow
+from  mlflow.tracking import MlflowClient
+
+
+
+
+# self.experiment_name: name of the experiment
+# self.mlflow_run: RUN object (class mlflow.entities.Run). 
+#   mlflow.entities.Run: contains the attribute .Data that contains
+#   the run data, including metrics, parameters, and tags.
+
+
+class Trainer():
+	MLFLOW_URI = "https://mlflow.lewagon.co/"
+
+	def __init__(self, experiment_name):
+		self.experiment_name = experiment_name
+  
+	@memoized_property
+	def mlflow_client(self):
+     # Generates the MlflowClient object that will be used below, memoized allows to 
+     # create only one object in despite of the multiple calls
+		mlflow.set_tracking_uri(self.MLFLOW_URI)
+		return MlflowClient()
+
+	@memoized_property
+	def mlflow_experiment_id(self):
+		try:
+			return self.mlflow_client.create_experiment(self.experiment_name)
+		except BaseException:
+			return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    def mlflow_create_run(self):
+        # creates the attribute mlflow_run
+		self.mlflow_run = self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+	def mlflow_log_param(self, key, value):
+		self.mlflow_client.log_param(self.mlfow_run.info.run_id, key, value)
+
+	def mlflow_log_metric(self, key, value):
+		self.mlflow_client.log_metric(self.mlfow_run.info.run_id, key, value)
+
+    def train(self):
+        for model in ["linear"]:
+            
+            set_pipline
+            
+            self.mlflow_create_run()
+            self.mlflow_log_metric("rmse", 4.5)
+            self.mlflow_log_param("model", model)
+
+trainer = Trainer("[FR] [Bordeaux] [sanpigh] linear_regression v1")
+trainer.train()
+
+
+
 # implement train() function
 def train(X_train, y_train, pipeline):
     '''returns a trained pipelined model'''
